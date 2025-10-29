ebbac490f833f933e0034da08c34f75cfabf8f8c
Pete Keen <pete@petekeen.net>
2025-10-29 16:52:42 -0400

    Hm

--- stacks/lib/adsb-bubble.rb
+++
@@ -1,6 +0,0 @@
-module AdsbBubble
-  autoload :Aircraft, 'adsb-bubble/aircraft.rb'
-  autoload :Tracker, 'adsb-bubble/tracker.rb'
-  autoload :Web, 'adsb-bubble/web.rb'
-  autoload :Notify, 'adsb-bubble/notify.rb'
-end
